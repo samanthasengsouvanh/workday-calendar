011399582037b807d1aac8c90b417636d50f895e
Samantha Sengsouvanh <samantha.sengsouvanh@gmail.com>
2019-11-21 20:07:19 -0600

    updated js with pseudo code

---
+++ Assets/js/script.js
@@ -0,0 +1,16 @@
+//function to display time and date at top of page
+
+function currentDay(moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+const currentDAy
+console.log("moment")
+//get the element that the info goes into
+//decide what it should look like
+//and then update it
+//should this run in a loop and update the clock regularly
+//create timeblocks with inputs and save buttons
+//loop and dynamically or hard code
+//add events to the submit button to save what is in the input to local storage
+//get the value out of the local storage and populate input with the data
+//check color coordinator based on time moment
+//look at css for classes
+//store data in local storage as object where the key is the hour of the day
